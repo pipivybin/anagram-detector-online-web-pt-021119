a5d1d3f53ff229fc9f3794f8251c296e7bccb044
Ming Duan <11203duan@gmail.com>
2019-03-19 20:08:58 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,14 @@ class Anagram
   end
 
   def match(range)
+    @sth.each do
+      |x| range.each do
+        |y| if y.length == x.length && y.split("").include?(x)
+          y
+          end
+        end
+      end
+    end
 
-  end
 
 end
