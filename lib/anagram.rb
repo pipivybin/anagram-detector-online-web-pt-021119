7311330714ca7bd5a79edd93bf72206c1836fac5
Ming Duan <11203duan@gmail.com>
2019-03-19 20:13:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,15 @@ class Anagram
   end
 
   def match(range)
-    arr = range.split(" ")
+    if range.class == arr
+      @sth.split("").each do
+        |x| range.each do
+          |y| if y.length == x.length && y.split("").include?(x)
+            y
+            end
+          end
+        end
+      else arr = range.split(" ")
     @sth.split("").each do
       |x| arr.each do
         |y| if y.length == x.length && y.split("").include?(x)
