1601d7e2812d5c28c9e6cd00fd09d9499c91402e
Ming Duan <11203duan@gmail.com>
2019-03-19 20:55:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  def initialize(testing_word)
+    @testing_word = testing_word
+  end
+
+  def match(range)
+    range.select do
+      |x| x.split("").sort == @testing_word.split("").sort
+    end
+  end
+
+
+
+end
