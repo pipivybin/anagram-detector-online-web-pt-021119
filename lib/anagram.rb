c0cb2517195162d7819b64d95402e7c7b865ffac
Ming Duan <11203duan@gmail.com>
2019-03-19 20:20:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,28 +2,17 @@ class Anagram
 
   attr_accessor
 
-  def initialize(sth)
-    @sth = sth
+  def initialize(testing_word)
+    @testing_word = testing_word
   end
 
   def match(range)
     if range.class == Array
-      @sth.split("").each do
-        |x| range.each do
-          |y| if y.length == x.length && y.split("").include?(x)
-            y
-            end
-          end
-        end
-      else arr = range.split(" ")
-    @sth.split("").each do
-      |x| arr.each do
-        |y| if y.length == x.length && y.split("").include?(x)
-          y
-          end
-        end
-      end
-    end
+
+
+    else
+      
+
   end
 
 
