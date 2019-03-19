bbf1d30fa965b6127ecb20d40b46b4e97480fccf
Ming Duan <11203duan@gmail.com>
2019-03-19 20:11:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,8 @@ class Anagram
   end
 
   def match(range)
-    @sth.each do
+    arr = range.split(" ")
+    @sth.split("").each do
       |x| range.each do
         |y| if y.length == x.length && y.split("").include?(x)
           y
